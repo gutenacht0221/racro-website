be530a89d45a3d1966aa66f20017e9334d5155e7
dxxxxy <introsdream@gmail.com>
2022-11-27 01:27:10 -0500

    style: changed background

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+console.log("dxxxxy#0776 was here...")
+
+particlesJS.load("particles-js", "particles.json", () => {
+    console.log("callback - particles.js config loaded");
+})
+
 function ReturnHome() {
     window.location.replace("../index.html");
 }
