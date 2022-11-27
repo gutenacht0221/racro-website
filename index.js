53953dff263b63d829b686f400425f32e6cecdbf
Gute Nacht <gutenacht0221@gmail.com>
2022-11-26 20:39:34 -0500

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function ReturnHome() {
     window.location.replace("../index.html");
 }
-
-function SendToSellix(){
-    window.location.replace("https://sellix.io/"); // This is a link to Sellix.io
-}
